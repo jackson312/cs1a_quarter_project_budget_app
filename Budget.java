5591ef7ccb598b517f519b3fb105aaaf2dbe47e4
Jackson Jones <jackson.jones@gmail.com>
2016-06-11 18:55:56 -0700

    Refactored app.

--- Budget.java
+++ Budget.java
@@ -2,33 +2,98 @@ import java.util.Scanner;
 
 public class Budget
 {
+    // Class, to be used as a storage class.
     public class Bill
     {
-        public double Amount;
-        public String Name;
+        public double Amount = 0.0;
+        public String Name = "";
     }
 
-    enum BillCategory_t {Rent, Electricity, Water, Food, Gas};
-    static private int count = 0;
-    static private Bill[] bills;
-    static private Scanner input;
+    private Scanner input = new Scanner(System.in);
+
+    // Using an enum type so that we can easily map the location of the category and its name.
+    private enum BillCategory_t
+    {
+        Food, Gas, Rent, Bills, Extra, Savings
+    }
+
+    private Bill[] budget = new Bill[BillCategory_t.values().length];
+    private Bill[] actuals = new Bill[BillCategory_t.values().length];
+
+
+    // Class Constructor, Initializes the two arrays with the storage class and sets the category names
+    Budget()
+    {
+        // Create class for budget and actuals and initialize them
+        for (BillCategory_t i : BillCategory_t.values())
+        {
+            budget[i.ordinal()] = new Bill();
+            budget[i.ordinal()].Name = i.toString();
+
+            actuals[i.ordinal()] = new Bill();
+            actuals[i.ordinal()].Name = i.toString();
+        }
+    }
+
+
+    public void clientEstimates()
+    {
+        //This method creates an array inputed with the Clients estimates for each category
+        //categories we have : [0] -> food, [1] -> bills, [2] -> extras
+        System.out.println("Please enter in the estimates for the categories asked below.");
+
+        for (BillCategory_t i : BillCategory_t.values())
+        {
+            System.out.print("Enter in your monthly estimate for " + i.toString() + ": ");
+            budget[i.ordinal()].Amount = input.nextDouble();
+            System.out.println();
+        }
+    }
+
+    private double calculateTotal(Bill[] list)
+    {
+        double total = 0.0;
+        for (BillCategory_t i : BillCategory_t.values())
+        {
+            total += list[i.ordinal()].Amount;
+        }
+        return total;
+    }
 
     public static void main(String[] args)
     {
-        Budget2 myBudget = new Budget2();
-        double total;
+        // We have to do this since the main is a statio function, we need at least one instance of the class
+        // so here it is.
+        Budget myBudget = new Budget();
+
+        // Calls clientEstimates
+        myBudget.clientEstimates();
+
+        // This could be moved to inside another helper function
+        for (BillCategory_t i : BillCategory_t.values())
+        {
+            System.out.printf("Category: %9s  Budgeted Amount: %8.2f  Actual is: %8.2f \n",
+                              myBudget.budget[i.ordinal()].Name,
+                              myBudget.budget[i.ordinal()].Amount,
+                              myBudget.actuals[i.ordinal()].Amount);
+        }
+
+        System.out.printf("The total for the actuals is: %8.2f \n",
+                          myBudget.calculateTotal(myBudget.budget));
+    }
+}
+
+
+//Junk code, saving in case it has something useful
+
+
+/*        double total;
         input = new Scanner(System.in);
         System.out.print("Please enter the number of bills you would like to add: ");
         bills = new Bill[input.nextInt()];
         input.nextLine();
 
-        for (BillCategory_t i: while (count < bills.length)
-        {
-            myBudget.printMenu();
-            myBudget.getExpenses(input.nextInt());
 
-            input.nextLine(); // Again clearing the buffer
-        }
 
         total = 0.0;
         for (Bill i: bills)
@@ -73,16 +138,4 @@ public class Budget
                 count++;
         input.nextLine();
     }
-
-    public void printMenu()
-    {
-        System.out.println("You will be prompted for the amount after you choose one of the following options:");
-        System.out.println("1. Rent");
-        System.out.println("2. Electricity");
-        /*
-         * Just keep printing them, I put them all on separate lines
-         * for management purposes, the escape for a new line is \n.
-         * You could do - "1. Rent\n2. Electricity\n3. ... " and so forth
-	     */
-    }
-}
+*/
